510596fde1e9464398c868327184df445b24070f
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-06-20 01:19:49 +0200

    deploy

--- app/config.js
+++ app/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    serviceName : 'room service',
-    serviceDescription : 'room service for create room, join room, leave room, end room, list rooms',
-    development : {
-        port : 3002,
-        url : 'http://localhost:3002'
+    serviceName: 'room service',
+    serviceDescription: 'room service for create room, join room, leave room, end room, list rooms',
+    development: {
+        port: 3002,
+        url: 'http://localhost:3002'
     },
-    production : {
-        port : process.env.PORT,
-        url : 'https://yalla-dardasha-user.herokuapp.com'
+    production: {
+        port: process.env.PORT,
+        url: 'https://yalla-dardasha-user.herokuapp.com'
     },
-    mongo : {
-        mongoDevelopmentUrl : 'mongodb://localhost/gp_clubhouse',
-        mongoProductionUrl : 'mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?retryWrites=true&w=majority',
-        options : {
+    mongo: {
+        mongoDevelopmentUrl: 'mongodb://localhost/gp_clubhouse',
+        mongoProductionUrl: 'mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?retryWrites=true&w=majority',
+        options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
         }
     },
-    devEnvironment : true
+    devEnvironment: false
 }
